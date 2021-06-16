bda3ac097a3b348e761de18de140543187fa0be4
yeomhs420 <69899248+yeomhs420@users.noreply.github.com>
2021-06-16 15:25:46 +0900

    Add files via upload

---
+++ StockWeb-main/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from main import bp as mainbp
+from check import bp as checkbp
+from statistics import bp as statisticsbp
+app.register_blueprint(mainbp)
+app.register_blueprint(checkbp)
+app.register_blueprint(statisticsbp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
